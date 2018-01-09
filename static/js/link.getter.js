f6c9c25159d74d6ec9756af500e944ebe8c4353b
Marise Hayashi <limitedeternity@users.noreply.github.com>
2018-01-09 09:43:37 +0300

    Update link.getter.js

--- static/js/link.getter.js
+++ static/js/link.getter.js
@@ -32,6 +32,7 @@ $(document).ready(function() {
         .fail(function(xhr, status, error) {
             $("#output").html('');
             window.open().document.write(xhr.responseText);
+            window.close();
         });
     });
 });
